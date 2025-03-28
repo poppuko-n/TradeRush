8ae8bfcdf126830af8696a9e0cb99cb1c84e2d92
poppuko-n <souta0609@icloud.com>
2025-03-28 14:08:22 +0900

    feat: 売値買値を取得し、表示する

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [askPrice, setAskPrice] = useState();
+  const [bidPrice, setBidPrice] = useState();
+  const BASE_URL = '/api/public/v1/ticker'; 
+
+
+  useEffect(() => {
+    axios.get(BASE_URL)
+      .then((response) => {
+        const usdJpy = response.data.data.find(item => item.symbol === 'USD_JPY');
+        if (usdJpy) {
+          setAskPrice(usdJpy.ask);
+          setBidPrice(usdJpy.bid);
+        }
+      })
+      .catch((error) => {
+        console.error('API取得エラー:', error);
+      });
+  }, []);
+
   return (
-    <div className="text-2xl font-bold text-blue-500">
-      Hello Tailwind ✨
+    <div className="text-2xl font-bold text-blue-500 p-4">
+      <div>Ask: {askPrice}</div>
+      <div>Bid: {bidPrice}</div>
     </div>
   );
 }
 
-export default App; 
+export default App;
