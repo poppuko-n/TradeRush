b9ea1ccfec21d31aabbaba14c14913cb6f48cd39
poppuko-n <souta0609@icloud.com>
2025-03-30 11:34:01 +0900

    feat: jwtトークンを発行し、データベースへの保存を実施

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json:{ token: create_token(user.id), status: :created}
+    else
+      render json: {
+        user: user,
+        errors: user.errors.full_messages
+      },
+      status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
