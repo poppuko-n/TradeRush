0a904aaa2df4f165413c7ffa8249e71e7259a1a8
poppuko-n <souta0609@icloud.com>
2025-04-10 21:52:32 +0900

    faet: レバレッジの導入

--- frontend/src/TradeAction.jsx
+++ frontend/src/TradeAction.jsx
@@ -21,11 +21,13 @@ const TradeAction = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [lot, setLot] = useState(1); 
   const [capital, setCapital] = useState(2000000);
+  const [leverage, setLeverage] = useState(1);
+
   
   // NOTE:買った価格（ask）と現在の売値（bid）で損益額を算出
   const displayProfitLoss = isTrade ? (bidPrice - handleAskPrice) * lot * 1000 : 0;
-  const profitLoss = (handleBidPrice - handleAskPrice) * lot * 1000;
-  const margin = lot * bidPrice * 5000;
+  const profitLoss = ((handleBidPrice - handleAskPrice) * lot * 1000).toFixed(2);
+  const margin = lot * bidPrice * 1000 / leverage;
   const isTradeAction = capital >= margin;
 
   // NOTE: 買い注文（売る準備）
@@ -74,7 +76,7 @@ const TradeAction = () => {
       <div className="flex flex-col items-center space-y-2">
 
       {/* NOTE: 取引中メッセージ */}
-      <div className="w-full h-10 flex items-center justify-center mt-2 mb-10">
+      <div className="w-full h-10 flex items-center justify-center mt-2 mb-6">
         {isTrade && (
           <div className="w-full bg-red-500 text-white text-lg text-center font-semibold px-4 py-1 rounded">
             取引中
@@ -105,7 +107,7 @@ const TradeAction = () => {
 
         </div>
   
-        <div className="border border-black rounded-2xl py-4 px-8 flex flex-col gap-16 w-full max-w-md shadow-2xl">
+        <div className="border border-black rounded-2xl py-2 px-8 flex flex-col gap-12 w-full max-w-md shadow-2xl">
           {/* 保有資産 */}
           <div className="text-black text-2xl py-3 w-full text-left">
             保有資産: {Math.floor(capital).toLocaleString()}
@@ -123,7 +125,7 @@ const TradeAction = () => {
           {/* ロット数入力 */}
           <div className="w-full">
             <label className="text-2xl flex items-center gap-4">
-              <span>取引ロット数：</span>
+              <span>取引ロット：</span>
               <img 
               src={MinusButton} 
               alt="マイナスボタン"
@@ -134,6 +136,25 @@ const TradeAction = () => {
               src={AddButton} 
               alt="プラスボタン"
               onClick={ ()=> setLot(prev=>(prev+1))}
+              className='h-8 w-8 cursor-pointer bg-gray-300 rounded p-1 transition duration-700" '
+              />
+            </label>
+          </div>
+
+          {/* レバレッジ入力 */}
+          <div className="w-full">
+            <label className="text-2xl flex items-center gap-4">
+              <span>レバレッジ：</span>
+              <img 
+              src={MinusButton} 
+              alt="マイナスボタン"
+              onClick={() => setLeverage(prev => Math.max(1, prev-1))} 
+              className=' h-8 w-8 cursor-pointer bg-gray-300 rounded p-1 transition duration-700" '/>
+              <p>{leverage}</p>
+              <img 
+              src={AddButton} 
+              alt="プラスボタン"
+              onClick={ ()=> setLeverage(prev=> Math.min(10, prev+1))}
               className='h-8 w-8 cursor-pointer bg-gray-300 rounded p-1 transition duration-700" '
               />
             </label>
@@ -152,38 +173,36 @@ const TradeAction = () => {
           </p>
   
           {/* 注文ボタン */}
-{/* 注文ボタンエリア */}
-<div className="flex flex-col items-center min-h-[56px] space-y-2">
-  {/* 警告メッセージ */}
-  <p className={`text-red-500 text-center text-sm transition-opacity duration-300 ${
-    isTradeAction ? 'opacity-0' : 'opacity-100'
-  }`}>
-    保有資金内でロット数を選択してください。
-  </p>
+          <div className="flex flex-col items-center min-h-[56px] space-y-2">
+            {/* 警告メッセージ */}
+            <p className={`text-red-500 text-center text-sm transition-opacity duration-300 ${
+              isTradeAction ? 'opacity-0' : 'opacity-100'
+            }`}>
+              保有資金内でロット数を選択してください。
+            </p>
 
-  {/* ボタン */}
-  {isTrade ? (
-    <button 
-      onClick={handleBid}
-      className="bg-black hover:bg-gray-600 text-white px-6 py-2 rounded-md"
-    >
-      売却
-    </button>
-  ) : (
-    <button 
-      onClick={handleAsk}
-      disabled={!isTradeAction}
-      className={`px-6 py-2 rounded-md text-white transition ${
-        isTradeAction
-          ? 'bg-black'
-          : 'bg-gray-400 cursor-not-allowed'
-      }`}
-    >
-      注文
-    </button>
-  )}
-</div>
-
+            {/* ボタン */}
+            {isTrade ? (
+              <button 
+                onClick={handleBid}
+                className="bg-black hover:bg-gray-600 text-white px-6 py-2 rounded-md"
+              >
+                売却
+              </button>
+            ) : (
+              <button 
+                onClick={handleAsk}
+                disabled={!isTradeAction}
+                className={`px-6 py-2 rounded-md text-white transition ${
+                  isTradeAction
+                    ? 'bg-black'
+                    : 'bg-gray-400 cursor-not-allowed'
+                }`}
+              >
+                注文
+              </button>
+            )}
+          </div>
         </div>
       </div>
 
@@ -207,7 +226,7 @@ const TradeAction = () => {
                 : profitLoss < 0
                 ? 'text-blue-600'
                 : 'text-black'}>
-                {profitLoss.toFixed(0)} 円
+                {Math.floor(profitLoss).toLocaleString()} 円
               </span>
             </p>
             <p className='text-xl'>買値：{handleAskPrice}</p>
