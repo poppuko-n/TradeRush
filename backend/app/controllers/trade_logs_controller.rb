78c74e9cb97796ef076928d823052f5ecf84fc7a
poppuko-n <souta0609@icloud.com>
2025-04-13 02:35:14 +0900

    feat: 損益額を総資産へ反映

--- backend/app/controllers/trade_logs_controller.rb
+++ backend/app/controllers/trade_logs_controller.rb
@@ -2,12 +2,15 @@ class TradeLogsController < ApplicationController
   before_action :authenticate_user, {only: [:create]}
 
   def create
-    profit = params[:profitLoss]
+    profit = params[:profitLoss].to_f
     TradeLog.create!(
       user: @current_user,
       profit_loss: profit
     )
+    @current_user.update_columns(
+      capital: (@current_user.capital + profit).to_i
+    )
 
-    render json:{ message: "トレードログを保存しました。", profit_loss: profit}, status: :created
+    render json:{ message: "トレードログと資産を更新しました。"}, status: :created
   end
 end
