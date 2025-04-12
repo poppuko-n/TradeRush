02fb3d6e40916f79192b67a93787ee58c23fb607
poppuko-n <souta0609@icloud.com>
2025-04-12 21:35:11 +0900

    refactor: Userのnameにunique制約を付与

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
 end
---
+++ backend/db/migrate/20250412123136_add_unique_index_to_name_in_users.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToNameInUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_index :users, :name, unique:true
+  end
+end
