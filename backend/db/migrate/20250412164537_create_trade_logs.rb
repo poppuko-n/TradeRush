6b9d981727fa88bdc6c3f9002d402a9ec39e2c5a
poppuko-n <souta0609@icloud.com>
2025-04-13 01:54:53 +0900

    refactor: profit_lossのカラム名を修正

--- backend/db/migrate/20250412164537_create_trade_logs.rb
+++ backend/db/migrate/20250412164537_create_trade_logs.rb
@@ -2,7 +2,7 @@ class CreateTradeLogs < ActiveRecord::Migration[8.0]
   def change
     create_table :trade_logs do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :profit_lossm, null:false
+      t.integer :profit_loss, null:false
 
       t.timestamps
     end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[8.0].define(version: 2025_04_12_164537) do
   create_table "trade_logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.integer "profit_lossm", null: false
+    t.integer "profit_loss", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_trade_logs_on_user_id"
